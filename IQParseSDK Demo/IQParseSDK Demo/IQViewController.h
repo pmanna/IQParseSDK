b3e2b9b6303e04b5c0a2e51d29b4131f970ba8ad
Iftekhar <iftekhar@Iftekhars-Mac-Pro.local>
2014-09-06 01:26:51 +0530

    Added files

---
+++ IQParseSDK Demo/IQParseSDK Demo/IQViewController.h
@@ -0,0 +1,13 @@
+//
+//  IQViewController.h
+//  IQParseSDK Demo
+//
+//  Created by Iftekhar on 06/09/14.
+//  Copyright (c) 2014 Iftekhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IQViewController : UIViewController
+
+@end
